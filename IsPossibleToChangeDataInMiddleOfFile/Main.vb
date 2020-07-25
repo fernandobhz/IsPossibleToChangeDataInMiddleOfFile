036b70970f243266303c3231a0f16bc7103b8143
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 19:09:27 -0300

    old project migrated

---
+++ IsPossibleToChangeDataInMiddleOfFile/Main.vb
@@ -0,0 +1,20 @@
+﻿Imports System.IO
+
+Module Main
+
+    Sub Main()
+
+        Dim Lock As New FileStream("C:\temp\x.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+
+        Dim F As New FileStream("C:\temp\x.txt", FileMode.Open, FileAccess.Write, FileShare.Read)
+        F.Seek(5, SeekOrigin.Begin)
+
+        Dim Buff() As Byte = System.Text.Encoding.UTF8.GetBytes("X")
+        F.Write(Buff, 0, Buff.Length)
+        F.Close()
+
+        MsgBox("Done")
+
+    End Sub
+
+End Module
